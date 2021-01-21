4f9e4b22ed2af601ada6eebb6de1100446cd64fb
Simzahn <unconfigured@null.spigotmc.org>
2021-01-21 08:30:37 +0100

    first Commit
    Just all the basic files

---
+++ src/main/java/me/simzahn/minecraftdays/MinecraftDays.java
@@ -0,0 +1,16 @@
+package me.simzahn.minecraftdays;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MinecraftDays extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
